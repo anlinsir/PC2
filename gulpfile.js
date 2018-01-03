ca42c7ce0f302adbb406c789d572e7ee01898349
runoob <test@runoob.com>
2018-01-03 23:47:25 +0800

    三

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 var gulp = require("gulp"),
 	
-	livere =require("gulp-livereload"),
-	sass = require("gulp-sass")
+	sass = require("gulp-sass"),
+	livereload = require("gulp-livereload")
+	
 	
 
-gulp.task("sass", function(){
+gulp.task("sass",function(){
 	gulp.src("src/sass/*.scss")
-		.pipe(sass({outputStyle: 'expanded'}))
-		.pipe(gulp.dest("dist/css/"))
-		.pipe(livereload());
-});
+		.pipe(sass())
+		.pipe(gulp.dest("dist/sass"))
+		.pipe(livereload())
+	
+})
 
-gulp.task("watch", function(){
-	livereload.listen();
-	gulp.watch("sass/*.scss", ["sass"]);
-});
-
+gulp.task("watch",function(){
+	
+	livereload.listen()
+	
+	gulp.watch("src/sass/*.scss",["sass"])
+	
+})
 
