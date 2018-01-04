d53377f4c41e5556b9a550faf3babe5b09a3a4df
an <test@runoob.com>
2018-01-04 21:53:05 +0800

    1.4

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,8 @@ require.config({
 	paths:{
 		"jquery":"lib/jquery/jquery-1.12.4.min",
 		"lode":"js/headerfootertools",
-		"cookie":"lib/jquery_plugins/jquery.cookie"
+		"cookie":"lib/jquery_plugins/jquery.cookie",
+		"index":"js/index"
 	}
 	
 	
--- src/js/headerfootertools.js
+++ src/js/headerfootertools.js
@@ -1,5 +1,5 @@
 //定义模块  载入 header和footer的文件
-define(["jquery","cookie"],function($){
+define(["jquery","cookie","index"],function($){
 	$.ajax("html/include/header.html").done(function(data){
 		$("header").html(data)
 	}).done(function(){
@@ -13,6 +13,19 @@ define(["jquery","cookie"],function($){
 	
 	
 	
-	$("footer").load("html/include/footer.html")
+	$.ajax("html/include/footer.html").done(function(data){
+		$("footer").html(data)
+	}).done(function(){
+		$(".footer-img li:nth-child(3) a").mouseenter(function(){
+			$(".footer-img li:nth-child(3) img").fadeIn(500)
+		})
+		$(".footer-img li:nth-child(3)").mouseleave(function(){
+			$(".footer-img li:nth-child(3) img").fadeOut(500)
+			
+		})
+	})
+	
+	
+		
 })
 
